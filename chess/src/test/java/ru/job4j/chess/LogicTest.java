abb1faeb5f83c45b4d13e95684650350d9234442
Michael Dedun <michael.dedun@mail.ru>
2020-03-29 13:37:58 +0300

    Задание [#241000

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -1,4 +1,21 @@
 package ru.job4j.chess;
 
+import org.junit.Assert;
+import org.junit.Test;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.black.BishopBlack;
+
 public class LogicTest {
+    
+    @Test
+    public void testMoveWhenTrue() {
+        Cell source = Cell.findBy(2, 7);
+        Cell dest = Cell.findBy(4, 5);
+        Logic logic = new Logic();
+        BishopBlack bishopBlack = new BishopBlack(source);
+        logic.add(bishopBlack);
+        boolean result = logic.move(source, dest);
+        Assert.assertTrue(result);
+    }
+
 }
--- chess/src/test/java/ru/job4j/chess/figures/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/figures/black/BishopBlackTest.java
@@ -1,4 +1,30 @@
 package ru.job4j.chess.figures.black;
 
+import org.junit.Assert;
+import org.junit.Test;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.black.BishopBlack;
+
 public class BishopBlackTest {
+
+    @Test
+    public void testWay() {
+        Cell source = Cell.findBy(2, 7);
+        Cell dest = Cell.findBy(4, 5);
+        BishopBlack bishopBlack = new BishopBlack(source);
+        Cell[] result = bishopBlack.way(source, dest);
+        Assert.assertEquals(Cell.findBy(3, 6), result[0]);
+        Assert.assertEquals(Cell.findBy(4, 5), result[1]);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testWrongWay() {
+        Cell source = Cell.findBy(2, 7);
+        Cell dest = Cell.findBy(4, 0);
+        BishopBlack bishopBlack = new BishopBlack(source);
+        Cell[] result = bishopBlack.way(source, dest);
+    }
+
+
+
 }
